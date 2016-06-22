104a562cb4761cef11a56f0dc2eb411bf27e395e
Chase Burnham <cburnhamiv@gmail.com>
2016-06-21 21:24:25 -0400

    Added add stretch dialog

--- app/src/main/java/letshangllc/stretchingroutines/Activities/CreateRoutineActivity.java
+++ app/src/main/java/letshangllc/stretchingroutines/Activities/CreateRoutineActivity.java
@@ -1,15 +1,47 @@
 package letshangllc.stretchingroutines.Activities;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.Locale;
 
 import letshangllc.stretchingroutines.R;
+import letshangllc.stretchingroutines.dialogs.AddStretchDialog;
 
 public class CreateRoutineActivity extends AppCompatActivity {
+    private static final String TAG = CreateRoutineActivity.class.getSimpleName();
 
+    /* ListView for the stretches */
+    private ListView lvStretches;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_routine);
+        setupViews();
     }
+
+    public void setupViews(){
+        lvStretches = (ListView) findViewById(R.id.lvStretches);
+
+
+    }
+
+    public void addStretchOnClick(View view){
+        AddStretchDialog addStretchDialog = new AddStretchDialog();
+
+        addStretchDialog.setCallback(new AddStretchDialog.Listener() {
+            @Override
+            public void onDialogPositiveClick(String name, int duration, String description) {
+
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/letshangllc/stretchingroutines/dialogs/AddStretchDialog.java
@@ -0,0 +1,64 @@
+package letshangllc.stretchingroutines.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import letshangllc.stretchingroutines.R;
+
+/**
+ * Created by cvburnha on 6/21/2016.
+ */
+public class AddStretchDialog extends DialogFragment {
+    /* Callback for when user presses Add */
+    private  Listener mListener;
+
+    public interface Listener {
+        void onDialogPositiveClick(String name, int duration, String description);
+    }
+
+    public void setCallback(Listener mListener) {
+        this.mListener = mListener;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        View view = inflater.inflate(R.layout.dialog_add_routine, null);
+
+        final EditText etDuration = (EditText) view.findViewById(R.id.etStretchDuration);
+        final EditText etDescription = (EditText) view.findViewById(R.id.etStretchDescription);
+        final EditText etName = (EditText) view.findViewById(R.id.etStretchName);
+
+
+        builder.setView(view)
+                // Add action buttons
+                .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        int duration = Integer.parseInt(etDuration.getText().toString());
+                        String description = etDescription.getText().toString();
+                        String name = etName.getText().toString();
+                        mListener.onDialogPositiveClick(name, duration, description);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        AddStretchDialog.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+
+
+}
