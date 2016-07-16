e9c2f97a745b11f5c2185ed2d2ca2240536ab160
Carl Burnham <cburnham@letshang.us>
2016-07-16 15:43:31 -0400

    Added delete routine

--- app/src/main/java/letshangllc/stretchingroutines/Activities/MainActivity.java
+++ app/src/main/java/letshangllc/stretchingroutines/Activities/MainActivity.java
@@ -1,17 +1,23 @@
 package letshangllc.stretchingroutines.Activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Timer;
@@ -39,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = (ListView) findViewById(R.id.lv_routines);
-
+        registerForContextMenu(listView);
         //this.runAds();
 
     }
@@ -126,7 +132,76 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         this.addRoutinesFromDB();
 
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_delete_edit, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.edit:
+                editRoutine(routineItems.get((int) info.id));
+                return true;
+            case R.id.delete:
+                confirmDelete(routineItems.get((int) info.id));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void editRoutine(RoutineItem routineItem){
+
+    }
+
+    private void confirmDelete(final RoutineItem routineItem){
+        if(routineItem.id > 9900){
+            Toast.makeText(this, "You cannot delete this routine", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.delete_confirm));
+
+        builder.setPositiveButton(getString(R.string.delete), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                deleteRoutine(routineItem);
+            }
+        }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void deleteRoutine(RoutineItem routineItem){
+        StretchesDBHelper dbHelper = new StretchesDBHelper(this);
+        int routineId = routineItem.id;
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        /* Delete from Exercise table and routines table by using exercise id */
+        db.delete(DBTableConstants.ROUTINE_STRETCH_TABLE, DBTableConstants.ROUTINE_ID + " = " + routineId, null);
+        db.delete(DBTableConstants.ROUTINE_TABLE_NAME, DBTableConstants.ROUTINE_ID + " = " + routineId, null);
+
+        db.close();
+
+        /* Remove item from list and update list view */
+        routineItems.remove(routineItem);
+        routineListAdapter.notifyDataSetChanged();
     }
 }
